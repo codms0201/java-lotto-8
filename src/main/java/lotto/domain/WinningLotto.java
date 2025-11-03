b55ee919d62b0b832b04bf1ce803a2c3a947a736
codms0201 <chanyeol0615@naver.com>
2025-11-03 23:03:40 +0900

    feat(domain): 당첨 번호 관리를 위한 WinningLotto 클래스 추가
    - 당첨 번호와 보너스 번호 저장
    - 보너스 번호가 당첨 번호와 중복되지 않도록 검증
    - 사용자 로또와 비교하여 등수 판별 로직 구현

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validateBonusNumber(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Rank match(Lotto lotto) {
+        int matchCount = winningNumbers.countMatch(lotto);
+        boolean matchBonus = lotto.contains(bonusNumber);
+        return Rank.valueOf(matchCount, matchBonus);
+    }
+}
